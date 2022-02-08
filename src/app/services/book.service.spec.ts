335e73c291d1689e9f39ba4351311f3e5f1f5f77
santt31 <santt31@gmail.com>
2022-02-08 23:08:51 +0100

    pretty

--- src/app/services/book.service.spec.ts
+++ src/app/services/book.service.spec.ts
@@ -31,53 +31,55 @@ const listCartBook: Book[] = [
 
 ];
 
-describe('Test Service',()=>{
-    let service:BookService;
+describe('Test Service', () => {
+    let service: BookService;
     let httpMock: HttpTestingController;
-    let storage= {};
+    let storage = {};
 
-    beforeEach(()=>{
+    beforeEach(() => {
         TestBed.configureTestingModule({
-          imports: [ HttpClientTestingModule]  ,
-          providers: [ BookService],
-          schemas:[CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
+            imports: [HttpClientTestingModule],
+            providers: [BookService],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
         });
     });
 
-    beforeEach(()=>{
+    beforeEach(() => {
         service = TestBed.inject(BookService);
         httpMock = TestBed.inject(HttpTestingController);
         //agrego un espia para localstorage
-        spyOn(localStorage, 'getItem').and.callFake((key: string)=>{
-            return storage[key]? storage[key]: null;
-        })
+        spyOn(localStorage, 'getItem').and.callFake((key: string) => {
+            return storage[key] ? storage[key] : null;
+        });
     });
 
-    afterEach(()=>{
+    afterEach(() => {
         httpMock.verify();
     });
 
-    it('should be created',()=>{
+    it('should be created', () => {
         expect(service).toBeTruthy();
     });
 
-    it('getBook return a list of book and does a get method ', ()=>{
-        service.getBooks().subscribe((resp: Book[])=>{
+    it('getBook return a list of book and does a get method ', () => {
+        service.getBooks().subscribe((resp: Book[]) => {
             expect(resp).toEqual(listCartBook);//2. aqui evalue que lo que me llega es igual a listartBook
-        })
+        });
 
         const req = httpMock.expectOne(environment.API_REST_URL + `/book`);
         expect(req.request.method).toBe('GET');
-        
+
         //Simular la peticion http, mokeo el return de http.get
         req.flush(listCartBook);
     });
 
 
-  it('getBookFromCart return empty array when localStorage is empty',()=>{
-    const listBook = service.getBooksFromCart();
-    //creamos el spia, pq si no se crea esta consultando al localstorageReal.
-    expect(listBook.length).toBe(0);
-  });
+    it('getBookFromCart return empty array when localStorage is empty', () => {
+        const listBook = service.getBooksFromCart();
+        //creamos el spia, pq si no se crea esta consultando al localstorageReal.
+        expect(listBook.length).toBe(0);
+    });
+
+    
 
 });
